751961806df6a2f97308c97bd3b92404d44b2725
hamza <hamzasidd3634@gmail.com>
2023-09-19 00:54:01 +0500

    roll back

--- lib/src/suggestion/widget.dart
+++ lib/src/suggestion/widget.dart
@@ -4,117 +4,130 @@ import 'package:rich_text_view/rich_text_view.dart';
 class SearchItemWidget extends StatelessWidget {
   final SuggestionController suggestionController;
   final TextEditingController? controller;
+  final Color? suggestionColor;
+  final Color? backgroundColor;
   final Function(TextEditingController)? onTap;
+
   final EdgeInsets? padding;
   final EdgeInsets? margin;
   final double radius;
   SearchItemWidget({
     required this.suggestionController,
     this.controller,
-    this.onTap,
-    this.padding,
+    this.suggestionColor,
+    this.backgroundColor,
+    this.onTap,        this.padding,
     this.margin,
     this.radius =20,
+
+
   });
 
   @override
   Widget build(BuildContext context) {
     var state = suggestionController.state;
-    var border = BorderSide(
-        width: Theme.of(context).brightness == Brightness.dark ? 0.1 : 1.0,
-        color: state.suggestionHeight > 1
-            ? Colors.grey[200]!
-            : Colors.transparent);
+    // var border = BorderSide(
+    //     width: Theme.of(context).brightness == Brightness.dark ? 0.1 : 1.0,
+    //     color:
+    //         state.suggestionHeight > 1 ? suggestionColor! : Colors.transparent);
     return Container(
         constraints: BoxConstraints(
-          minHeight: 1,
+          minHeight: 0,
           maxHeight: state.suggestionHeight,
           maxWidth: double.infinity,
           minWidth: double.infinity,
         ),
         decoration: BoxDecoration(
-            color: Theme.of(context).cardColor,
-            border: Border(
-              top: suggestionController.position == SuggestionPosition.top &&
-                      state.suggestionHeight > 1.0
-                  ? border
-                  : BorderSide.none,
-              left: border,
-              right: border,
-              bottom: border,
-            )),
+          color: state.suggestionHeight == 1
+              ? Colors.transparent
+              : backgroundColor,
+          // border: Border(
+          //   top: suggestionController.position == SuggestionPosition.top &&
+          //           state.suggestionHeight > 1.0
+          //       ? border
+          //       : BorderSide.none,
+          //   left: border,
+          //   right: border,
+          //   bottom: border,
+          // )
+        ),
         child: state.loading
             ? Padding(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: Center(
-                  child: SizedBox(
-                      width: 30,
-                      height: 30,
-                      child: CircularProgressIndicator()),
-                ))
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Center(
+              child: SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircularProgressIndicator()),
+            ))
             : Scrollbar(
-                thickness: 3,
-                child: state.last.startsWith(suggestionController.mentionSymbol)
-                    ? ListView.builder(
-                        itemCount: state.mentions.length,
-                        itemBuilder: (context, index) {
-                          var mention = state.mentions[index];
-                          if (mention == null) return SizedBox();
+          thickness: 3,
+          child: state.last.startsWith(suggestionController.mentionSymbol)
+              ? ListView.builder(
+            padding: EdgeInsets.zero,
+            physics: AlwaysScrollableScrollPhysics(),
+            itemCount: state.mentions.length,
+            itemBuilder: (context, index) {
+              var mention = state.mentions[index];
+              if (mention == null) return SizedBox();
 
-                          return InkWell(
-                            onTap: () {
-                              var _controller = suggestionController.onuserselect(
-                                  '${suggestionController.mentionSymbol}${mention.subtitle}',
-                                  controller!);
-                              suggestionController.onMentionSelected
-                                  ?.call(mention);
-                              onTap!(_controller);
-                            },
-                            child: suggestionController.mentionSearchCard
-                                    ?.call(mention) ??
-                                ListUserItem(
-                                  title: mention.title,
-                                  subtitle: mention.subtitle,
-                                  imageUrl: mention.imageURL,
-                                    padding:padding,
-                                    margin:margin,
-                                    radius:radius,
-                                ),
-                          );
-                        },
-                      )
-                    : state.hashtags.isNotEmpty
-                        ? ListView.builder(
-                            itemBuilder: (context, position) {
-                              var item = state.hashtags[position];
-                              return InkWell(
-                                  onTap: () {
-                                    var _controller =
-                                        suggestionController.onuserselect(
-                                            '${item.hashtag}', controller!);
-                                    suggestionController.onHashTagSelected
-                                        ?.call(item);
-                                    onTap!(_controller);
-                                  },
-                                  child: suggestionController.hashTagSearchCard
-                                          ?.call(item) ??
-                                      ListTile(
-                                        onTap: null,
-                                        title: Text(
-                                          item.hashtag,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .subtitle2,
-                                        ),
-                                        subtitle: Text(item.subtitle ?? ''),
-                                        trailing: item.trending
-                                            ? Text('Trending')
-                                            : SizedBox(height: 0, width: 0),
-                                      ));
-                            },
-                            itemCount: state.hashtags.length)
-                        : SizedBox(),
-              ));
+              return InkWell(
+                onTap: () {
+                  var _controller = suggestionController.onuserselect(
+                      '${suggestionController.mentionSymbol}${mention.subtitle}',
+                      controller!);
+                  suggestionController.onMentionSelected
+                      ?.call(mention);
+                  onTap!(_controller);
+                },
+                child: suggestionController.mentionSearchCard
+                    ?.call(mention) ??
+                    ListUserItem(
+                      title: mention.title,
+                      backgroundColor: backgroundColor!,
+                      subtitle: mention.subtitle,
+                      imageUrl: mention.imageURL,
+                      padding: padding,
+                      radius: radius,
+                      margin: margin,
+                    ),
+              );
+            },
+          )
+              : state.hashtags.isNotEmpty
+              ? ListView.builder(
+              padding: EdgeInsets.zero,
+              physics: AlwaysScrollableScrollPhysics(),
+              itemBuilder: (context, position) {
+                var item = state.hashtags[position];
+                return InkWell(
+                    onTap: () {
+                      var _controller =
+                      suggestionController.onuserselect(
+                          '${item.hashtag}', controller!);
+                      suggestionController.onHashTagSelected
+                          ?.call(item);
+                      onTap!(_controller);
+                    },
+                    child: suggestionController.hashTagSearchCard
+                        ?.call(item) ??
+                        ListTile(
+                          onTap: null,
+                          title: Text(
+                            item.hashtag,
+                            style: Theme.of(context)
+                                .textTheme
+                                .subtitle2,
+                          ),
+                          subtitle: Text(item.subtitle ?? ''),
+                          trailing: item.trending
+                              ? Text('Trending')
+                              : SizedBox(height: 0, width: 0),
+                        ));
+              },
+              itemCount: state.hashtags.length)
+              : SizedBox(),
+        ));
   }
 }
 
@@ -122,25 +135,27 @@ class ListUserItem extends StatelessWidget {
   final String imageUrl;
   final String title;
   final String subtitle;
+  final Color backgroundColor;
+
+  ListUserItem(
+      {Key? key,
+        required this.imageUrl,
+        required this.backgroundColor,
+        required this.title,
+        required this.subtitle,
+        this.padding,
+        this.margin,
+        this.radius =20,
+      });
   final EdgeInsets? padding;
   final EdgeInsets? margin;
   final double radius;
 
 
-  ListUserItem(
-      {Key? key,
-      required this.imageUrl,
-      this.padding,
-      this.margin,
-      this.radius =20,
-      required this.title,
-
-      required this.subtitle});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:padding?? const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
       child: Row(children: <Widget>[
         CircleAvatar(
           backgroundImage: NetworkImage(imageUrl),
@@ -148,40 +163,41 @@ class ListUserItem extends StatelessWidget {
         ),
         Flexible(
             child: Container(
-          margin:margin?? EdgeInsets.only(left: 20.0, top: 8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
+              margin: EdgeInsets.only(left: 20.0, top: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    title.trim(),
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(fontWeight: FontWeight.w600),
-                    overflow: TextOverflow.ellipsis,
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        title.trim(),
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: backgroundColor == Colors.white
+                                ? Colors.black
+                                : Colors.white),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 2.0, right: 8.0),
+                    child: Container(
+                        child: Text(
+                          subtitle,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .caption!
+                              .copyWith(fontSize: 14),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                  )
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 2.0, right: 8.0),
-                child: Container(
-                    child: Text(
-                  subtitle,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context)
-                      .textTheme
-                      .caption!
-                      .copyWith(fontSize: 14),
-                )),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-              )
-            ],
-          ),
-        )),
+            )),
       ]),
     );
   }
--- lib/src/text_editor.dart
+++ lib/src/text_editor.dart
@@ -14,22 +14,26 @@ class RichTextEditor extends StatefulWidget {
   final TextAlign textAlign;
   final TextStyle? style;
   final bool autoFocus;
+  final bool expands;
+  final bool fromPost;
   final TextEditingController? controller;
   final InputDecoration? decoration;
   final Function(String)? onChanged;
   final int? maxLength;
   final int? minLines;
+  final Color? suggestionColor;
+  final Color? backgroundColor;
   final TextInputType? keyboardType;
   final FocusNode? focusNode;
   final bool readOnly;
   final TextDirection? textDirection;
+  final TextInputType? textInputType;
   final List<TextInputFormatter>? inputFormatters;
   final TextAlignVertical? textAlignVertical;
   final EdgeInsetsGeometry padding;
     final EdgeInsets? paddingSuggestion;
   final EdgeInsets? margin;
   final double radius;
-
   ///A controller for the suggestion behaviour and customisations.
   /// You can as well extend this controller for a more custom behaviour.
   final SuggestionController? suggestionController;
@@ -43,32 +47,36 @@ class RichTextEditor extends StatefulWidget {
 
   const RichTextEditor(
       {Key? key,
-      this.initialValue,
-      this.maxLines,
-      this.textAlign = TextAlign.start,
-      this.style,
-      this.controller,
-      this.decoration,
-            this.paddingSuggestion,
+        this.initialValue,
+        this.maxLines,
+        this.textAlign = TextAlign.start,
+        this.style,
+        this.controller,
+        this.decoration,
+        this.onChanged,
+        this.textInputType = TextInputType.text,
+        this.autoFocus = false,
+        this.fromPost = false,
+        this.maxLength,
+        this.minLines,
+        this.keyboardType,
+        this.focusNode,
+        this.suggestionColor,
+        this.readOnly = false,
+        this.expands = false,
+        this.suggestionController,
+        this.backgroundColor = Colors.white,
+        this.textInputAction,
+        this.textDirection,            this.paddingSuggestion,
     this.margin,
     this.radius =20,
-      this.onChanged,
-      this.autoFocus = false,
-      this.maxLength,
-      this.minLines,
-      this.keyboardType,
-      this.focusNode,
-      this.readOnly = false,
-      this.suggestionController,
-      this.textInputAction,
-      this.textDirection,
-      this.onEditingComplete,
-      this.inputFormatters,
-      this.textAlignVertical,
-      this.onFieldSubmitted,
-      this.onSaved,
-      this.validator,
-      this.padding = const EdgeInsets.only(top: 16.0)})
+        this.onEditingComplete,
+        this.inputFormatters,
+        this.textAlignVertical,
+        this.onFieldSubmitted,
+        this.onSaved,
+        this.validator,
+        this.padding = const EdgeInsets.only(top: 16.0)})
       : super(key: key);
 
   @override
@@ -85,10 +93,10 @@ class _RichTextEditorState extends State<RichTextEditor> {
     controller = widget.controller ??
         TextEditingController(text: widget.initialValue ?? '');
     suggestionController =
-        (widget.suggestionController ?? SuggestionController())
-          ..addListener(() {
-            setState(() {});
-          });
+    (widget.suggestionController ?? SuggestionController())
+      ..addListener(() {
+        setState(() {});
+      });
   }
   //
   // @override
@@ -104,7 +112,8 @@ class _RichTextEditorState extends State<RichTextEditor> {
       var searchItemWidget = SearchItemWidget(
           suggestionController: suggestionController,
           controller: controller,
-          padding:widget.paddingSuggestion,
+          backgroundColor: widget.backgroundColor,
+          suggestionColor: widget.suggestionColor,          padding:widget.paddingSuggestion,
           margin:widget.margin,
           radius:widget.radius,
           onTap: (contrl) {
@@ -114,13 +123,13 @@ class _RichTextEditorState extends State<RichTextEditor> {
           });
       return Padding(
         padding: widget.padding,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+        child: widget.fromPost == true
+            ? widget.maxLines == 8
+            ? Stack(
           children: [
-            if (suggestionController.position == SuggestionPosition.top)
-              searchItemWidget,
             TextFormField(
               style: widget.style,
+              expands: widget.expands,
               focusNode: widget.focusNode,
               controller: controller,
               textCapitalization: TextCapitalization.sentences,
@@ -147,7 +156,82 @@ class _RichTextEditorState extends State<RichTextEditor> {
               validator: widget.validator,
               onSaved: widget.onSaved,
             ),
-            if (suggestionController.position == SuggestionPosition.bottom)
+            Padding(
+              padding: const EdgeInsets.only(top: kToolbarHeight),
+              child: searchItemWidget,
+            )
+          ],
+        )
+            : Column(
+          children: [
+            TextFormField(
+              style: widget.style,
+              expands: widget.expands,
+              focusNode: widget.focusNode,
+              controller: controller,
+              textCapitalization: TextCapitalization.sentences,
+              readOnly: widget.readOnly,
+              textDirection: widget.textDirection,
+              textInputAction: widget.textInputAction,
+              onChanged: (val) async {
+                widget.onChanged?.call(val);
+                suggestionController
+                    .onChanged(val.split(' ').last.toLowerCase());
+              },
+              maxLines: widget.maxLines,
+              keyboardType: widget.keyboardType,
+              maxLength: widget.maxLength,
+              minLines: widget.minLines,
+              autofocus: widget.autoFocus,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              decoration: widget.decoration,
+              textAlign: widget.textAlign,
+              onFieldSubmitted: widget.onFieldSubmitted,
+              inputFormatters: widget.inputFormatters,
+              textAlignVertical: widget.textAlignVertical,
+              onEditingComplete: widget.onEditingComplete,
+              validator: widget.validator,
+              onSaved: widget.onSaved,
+            ),
+            searchItemWidget
+          ],
+        )
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            if (suggestionController.position == SuggestionPosition.top)
+              searchItemWidget,
+            TextFormField(
+              style: widget.style,
+              expands: widget.expands,
+              focusNode: widget.focusNode,
+              controller: controller,
+              textCapitalization: TextCapitalization.sentences,
+              readOnly: widget.readOnly,
+              textDirection: widget.textDirection,
+              textInputAction: widget.textInputAction,
+              onChanged: (val) async {
+                widget.onChanged?.call(val);
+                suggestionController
+                    .onChanged(val.split(' ').last.toLowerCase());
+              },
+              maxLines: widget.maxLines,
+              keyboardType: widget.keyboardType,
+              maxLength: widget.maxLength,
+              minLines: widget.minLines,
+              autofocus: widget.autoFocus,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              decoration: widget.decoration,
+              textAlign: widget.textAlign,
+              onFieldSubmitted: widget.onFieldSubmitted,
+              inputFormatters: widget.inputFormatters,
+              textAlignVertical: widget.textAlignVertical,
+              onEditingComplete: widget.onEditingComplete,
+              validator: widget.validator,
+              onSaved: widget.onSaved,
+            ),
+            if (suggestionController.position ==
+                SuggestionPosition.bottom)
               searchItemWidget,
           ],
         ),
